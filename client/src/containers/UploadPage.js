552e7e47b68b04dba40a73b355516a1a0129cf65
Brian Chao <whereisbrianchao520@gmail.com>
2020-05-07 19:59:32 +0800

    test ipfs and upload fail:

---
+++ client/src/containers/UploadPage.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+//import ipfs from '../../../ipfs/ipfsClient';
+
+const ipfsClient = require("ipfs-http-client");
+const ipfs = ipfsClient('http://localhost:5002');
+
+class UploadPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ipfsHash: null,
+            buffer: ''
+        }
+    }
+
+    onSubmit = async(e) => {
+        e.preventDefault();
+        // Send to ipfs server node
+        console.log(ipfs.getEndpointConfig());
+        await ipfs.add(this.state.buffer, (err, ipfsHash) =>{
+            this.setState({
+                ipfsHash: ipfsHash,
+            })
+            console.log(err, ipfsHash);
+        })
+        console.log("FINISH!");
+    }
+
+    onFileUpload = e => {
+        e.stopPropagation();
+        e.preventDefault();
+        const file = e.target.files[0];
+        // file.name = file original upload name
+        // file.size 
+        let reader = new window.FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onloadend = () => this.toBuffer(reader);
+    }
+
+    toBuffer = async(reader) => {
+        const buffer = await Buffer.from(reader.result);
+        this.setState({
+            buffer: buffer,
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <input 
+                        type = "file"
+                        onChange = {this.onFileUpload}
+                    />
+                    <button
+                        type="submit"
+                    > 
+                        Send file to ipfs
+                    </button>
+                </form>
+                <div>
+                    {this.state.ipfsHash}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UploadPage;
